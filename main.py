00f23bff22318f88fc6cf6088b570d513aaa50b0
Tibo Stroo <tibo.stroo@hotmail.be>
2022-06-30 16:02:38 +0200

    Shortest path algorithm

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+#   _____      _   _      __ _           _ _              __      ___                 _ _
+#  |  __ \    | | | |    / _(_)         | (_)             \ \    / (_)               | (_)
+#  | |__) |_ _| |_| |__ | |_ _ _ __   __| |_ _ __   __ _   \ \  / / _ ___ _   _  __ _| |_ _______ _ __
+#  |  ___/ _` | __| '_ \|  _| | '_ \ / _` | | '_ \ / _` |   \ \/ / | / __| | | |/ _` | | |_  / _ \ '__|
+#  | |  | (_| | |_| | | | | | | | | | (_| | | | | | (_| |    \  /  | \__ \ |_| | (_| | | |/ /  __/ |
+#  |_|   \__,_|\__|_| |_|_| |_|_| |_|\__,_|_|_| |_|\__, |     \/   |_|___/\__,_|\__,_|_|_/___\___|_|
+#                                                   __/ |
+#                                                  |___/
+
 import pygame
 from enum import Enum
+from queue import PriorityQueue
+import math
 
 
 class GridSquareState(Enum):
@@ -8,7 +19,8 @@ class GridSquareState(Enum):
     START = pygame.Color(255, 255, 0)
     END = pygame.Color(0, 255, 255)
     NO_PATH = pygame.Color(255, 0, 0)
-    PATH = pygame.Color(0, 255, 0)
+    POSSIBLE_PATH = pygame.Color(0, 255, 0)
+    DEFINITIVE_PATH = pygame.Color(0, 255, 255)
 
 
 class GridSquare:
@@ -44,6 +56,24 @@ class GridSquare:
     def make_empty(self):
         self.state = GridSquareState.EMPTY
         self.draw_and_update()
+
+    def make_possible_path(self):
+        self.state = GridSquareState.POSSIBLE_PATH
+        self.draw_and_update()
+
+    def make_no_path(self):
+        self.state = GridSquareState.NO_PATH
+        self.draw_and_update()
+
+    def make_path(self):
+        self.state = GridSquareState.DEFINITIVE_PATH
+        self.draw_and_update()
+
+    def is_barrier(self):
+        return self.state == GridSquareState.BARRIER
+
+    def __lt__(self, other):
+        return False
 
 
 class Grid:
@@ -88,6 +118,21 @@ class Grid:
             for x in range(self.rows):
                 self.grid[y][x].make_empty()
 
+    def get_neigbors(self, square):
+        x = square.x
+        y = square.y
+
+        out = []
+
+        for coord in [(x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1)]:
+            # for c in [x - 1, x, x + 1]:
+            #    for r in [y - 1, y, y + 1]:
+            if 0 <= coord[0] < self.rows and 0 <= coord[1] < self.rows:  # and not (x == c and y == r):
+                neighbor = self.grid[coord[1]][coord[0]]
+                if not neighbor.is_barrier():
+                    out.append(neighbor)
+        return out
+
 
 class Button:
     # https://github.com/russs123/pygame_tutorials/tree/main/Button
@@ -120,6 +165,60 @@ class Button:
             self.function()
 
 
+def reconstruct_path(came_from, current):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+
+
+def h(node, end):
+    # return math.sqrt((node.x - end.x) ** 2 + (node.y - end.y) ** 2)
+    return abs(node.x - end.x) + abs(node.y - end.y)
+
+
+def algorithm(grid, start, goal):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))  # f_score, count, node
+    open_set_values = {start}
+
+    came_from = {}
+
+    f_score = {square: float("inf") for row in grid.grid for square in row}
+    f_score[start] = h(start, goal)
+
+    g_score = {square: float("inf") for row in grid.grid for square in row}
+    g_score[start] = 0
+
+    while not open_set.empty():
+        current = open_set.get()[2]
+        open_set_values.remove(current)
+
+        if current == goal:
+            reconstruct_path(came_from, goal)
+            return
+
+        neighbors = grid.get_neigbors(current)
+
+        for n in neighbors:
+            tentative_g_score = g_score[current] + 1
+            if tentative_g_score < g_score[n]:
+                came_from[n] = current
+                g_score[n] = tentative_g_score
+                f_score[n] = tentative_g_score + h(n, goal)
+                if n not in open_set_values:
+                    count += 1
+                    open_set.put((f_score[n], count, n))
+                    open_set_values.add(n)
+                    n.make_possible_path()
+
+        if current != start:
+            current.make_no_path()
+
+    # goal never reached
+    return
+
+
 class Game:
 
     def __init__(self, rows, width):
@@ -129,6 +228,7 @@ class Game:
         self.width = width
         self.rows = rows
         self.window = pygame.display.set_mode((width, width + extra_space))
+        self.clock = pygame.time.Clock()
         pygame.display.set_caption("A* Path Finding Algorithm")
         self.grid = Grid(self.window, rows, width, width)
 
@@ -145,6 +245,9 @@ class Game:
         pygame.display.flip()
         pygame.display.update()
 
+        self.start = None
+        self.end = None
+
         self.play()
 
     def reload_animation(self):
@@ -154,13 +257,16 @@ class Game:
         self.play()
 
     def start_animation(self):
+        if self.start and self.end:
+            algorithm(self.grid, self.start, self.end)
         print("started animation")
 
     def play(self):
         run = True
 
-        start = None
-        end = None
+        self.start = None
+        self.end = None
+
         while run:
             self.play_btn.act()
             self.reload_btn.act()
@@ -176,13 +282,13 @@ class Game:
                     square = self.grid.get_square(row, col)
 
                     if square:
-                        if not start and square != end:
-                            start = square
-                            start.make_start()
-                        elif not end and square != start:
-                            end = square
-                            end.make_end()
-                        elif square != start and square != end:
+                        if not self.start and square != self.end:
+                            self.start = square
+                            self.start.make_start()
+                        elif not self.end and square != self.start:
+                            self.end = square
+                            self.end.make_end()
+                        elif square != self.start and square != self.end:
                             square.make_barrier()
 
                 if pygame.mouse.get_pressed()[2]:  # Right click
@@ -191,14 +297,15 @@ class Game:
                     square = self.grid.get_square(row, col)
 
                     if square:
-                        if square == start:
-                            start = None
-                        elif square == end:
-                            end = None
+                        if square == self.start:
+                            self.start = None
+                        elif square == self.end:
+                            self.end = None
                         square.make_empty()
 
             pygame.display.flip()
             pygame.display.update()
+            self.clock.tick(60)
 
         pygame.quit()
 
